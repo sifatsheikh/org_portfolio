5017134a7a9af84030d66f9018210226b5d532d3
sifatsheikh <thetopman776655@gmail.com>
2024-11-24 00:08:18 +0600

    completed all functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
 tailwind.config = {
     theme:{
-        gridTemplateColumns:{
+        extend:{
+          gridTemplateColumns:{
             'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+        },
+        fontFamily:{
+            outfit:["Outfit", "sans-serif"],
+             Ovo:["Ovo", "serif"]
+        },
+        animation:{
+            spin_slow:'spin 6s linear infinite'
+        },
+        colors:{
+            lightHover: '#f3f3f3',
+            darkHover : '#2a004a',
+            darkTheme : '#191a1b'
+        },
+        boxShadow:{
+            'black': '4px 4px 0 #000',
+            'white': '4px 4px 0 #000',
         }
-    }
+    }  
+},
+     darkMode: 'selector'
 }
