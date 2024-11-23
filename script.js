a6e55c5af1550aab96e825d17988e0201cda62b9
sifatsheikh <thetopman776655@gmail.com>
2024-11-23 15:03:16 +0600

    Design part completed and footer section added

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const sideMenu = document.querySelector('#sideMenu')
+const navBar = document.querySelector("nav");
+const navLinks = document.querySelector("nav ul");
 
 function openMenu(){
     sideMenu.style.transform = 'translatex(-16rem)';
@@ -6,3 +8,15 @@ function openMenu(){
 function colseMenu(){
     sideMenu.style.transform = 'translate(16rem)';
 }
+
+
+
+window.addEventListener('scroll', ()=>{
+    if(scrollY > 50){
+        navBar.classList.add('bg-white', 'bg-opacity-50','backdrop-blur-lg', 'shadow-sm')
+        navLinks.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50');
+    }else{
+        navBar.classList.remove('bg-white', 'bg-opacity-50','backdrop-blur-lg', 'shadow-sm')
+        navLinks.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50');
+    }
+}) 
